b2d5f1efd7ad20904a426fb702468d6e87d5b9cd
Darren Inglis <darren.inglis@gmail.com>
2019-06-08 10:30:59 +1000

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Card({ image, name, key }) {
-	// may need to make this a class to stop re render
-	let angle = Math.random() * 90 - 45;
-	let xPos = Math.random() * 40 - 20;
-	let yPos = Math.random() * 40 - 20;
-	let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+export default class Card extends Component {
+	constructor(props) {
+		super(props);
+		let angle = Math.random() * 90 - 45;
+		let xPos = Math.random() * 40 - 20;
+		let yPos = Math.random() * 40 - 20;
+		this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+	}
 
-	return (
-		<img className='Card' src={image} alt={name} style={{ transform }} />
-	);
+	render() {
+		return (
+			<img
+				className='Card'
+				src={this.props.image}
+				alt={this.props.name}
+				style={{ transform: this._transform }}
+			/>
+		);
+	}
 }
--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,66 +1,3 @@
-// import React, { Component } from 'react';
-// import Card from './Card';
-// import axios from 'axios';
-
-// const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
-
-// export default class Deck extends Component {
-// 	state = {
-// 		deck: null,
-// 		drawn: []
-// 	};
-
-// 	async componentDidMount() {
-// 		let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
-// 		this.setState({
-// 			deck: deck.data
-// 		});
-// 	}
-
-// 	async getCard() {
-// 		let id = this.state.deck.deck_id;
-// 		try {
-// 			let cardUrl = `${API_BASE_URL}/${id}/draw/`;
-// 			//make request using deck id
-// 			let cardRes = await axios.get(cardUrl);
-// 			if (!cardRes.data.success) {
-// 				throw new Error('No Cards remaining');
-// 			}
-
-// 			//set state using new card info
-// 			let card = cardRes.data.card[0];
-// 			this.setState((st) => ({
-// 				drawn: [
-// 					...st.drawn,
-// 					{
-// 						id: card.code,
-// 						image: card.image,
-// 						name: `${card.value} of ${card.suit}`
-// 					}
-// 				]
-// 			}));
-// 		} catch (err) {
-// 			alert(err);
-// 		}
-// 	}
-
-// 	render() {
-// 		const cards = this.state.drawn.map((c) => (
-// 			<Card name={c.name} image={c.image} key={c.id} />
-// 		));
-// 		return (
-// 			<div>
-// 				<h1 className='Deck-title'>Card Dealer</h1>
-// 				<h2 className='Deck-title subtitle'>
-// 					A little demo made with React
-// 				</h2>
-// 				<button onClick={this.getCard}>Get Card!</button>
-// 				<div className='Deck-cardarea'>{cards}</div>
-// 			</div>
-// 		);
-// 	}
-// }
-
 import React, { Component } from 'react';
 import Card from './Card';
 import axios from 'axios';
@@ -69,20 +6,30 @@ const API_BASE_URL = 'https://www.deckofcardsapi.com/api/deck';
 class Deck extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { deck: null, drawn: [] };
+		this.state = { deck: null, drawn: [], count: 0 };
 		this.getCard = this.getCard.bind(this);
+		this.updateCount = this.updateCount.bind(this);
 	}
+
 	async componentDidMount() {
 		let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
 		this.setState({ deck: deck.data });
+		this.getCard();
 	}
+
+	updateCount = () => {
+		this.setState({
+			count: this.state.count + 1
+		});
+	};
+
 	async getCard() {
 		let deck_id = this.state.deck.deck_id;
 		try {
 			let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
 			let cardRes = await axios.get(cardUrl);
-			if (!cardRes.data.success) {
-				throw new Error('No card remaining!');
+			if (this.state.count >= 52) {
+				throw new Error('End of Deck!');
 			}
 			let card = cardRes.data.cards[0];
 			console.log(cardRes.data);
@@ -96,8 +43,9 @@ class Deck extends Component {
 					}
 				]
 			}));
+			this.updateCount();
 		} catch (err) {
-			alert(err);
+			window.location.reload();
 		}
 	}
 
@@ -109,11 +57,12 @@ class Deck extends Component {
 			<div className='Deck'>
 				<h1 className='Deck-title'>♦ Card Dealer ♦</h1>
 				<h2 className='Deck-title subtitle'>
-					♦ A little demo made with React ♦
+					♦ A 52 card deck made with React ♦
 				</h2>
 				<button className='Deck-btn' onClick={this.getCard}>
 					Get Card!
 				</button>
+				<p>{`${this.state.count} / 52`}</p>
 				<div className='Deck-cardarea'>{cards}</div>
 			</div>
 		);
